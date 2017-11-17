f5749d3977dfcb3cdd9591f0b3bab9304acd5edd
lorris saint-genez <lorris.saint-genez@epita.fr>
2017-11-17 13:56:55 +0100

    [Home] Change LeVentTourne to Les Voisins

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Le Vent Tourne</title>
+        <title>Les Voisins</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -97,7 +97,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Le Vent Tourne
+                    Les Voisins
                 </div>
 
                 <div class="links">
